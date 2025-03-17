68f60d6c2a288db3a4fcbbb7cbde93f201e7c917
cuong <tranhungcuong31720@gmail.com>
2025-03-17 19:58:33 +0700

    update mainpage 17/3

---
+++ public/js/script.js
@@ -0,0 +1,22 @@
+const darkModeButton = document.getElementById("settingdarkmode");
+const header = document.querySelector("header");
+const nav = document.querySelector("nav");
+if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+    if (header) header.classList.add("dark-mode");
+    if (nav) nav.classList.add("dark-mode");
+    darkModeButton.textContent = "☀️"; 
+}
+darkModeButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    if (header) header.classList.toggle("dark-mode");
+    if (nav) nav.classList.toggle("dark-mode");
+    // Lưu trạng thái vào Local Storage
+    if (document.body.classList.contains("dark-mode") ) {
+        localStorage.setItem("darkMode", "enabled");
+        darkModeButton.textContent = "☀️"; 
+    } else {
+        localStorage.setItem("darkMode", "disabled");
+        darkModeButton.textContent = "🌙"; 
+    }
+});
